23575caae75fd2298e62d3be484b68e1104f0a18
emon <liming20110711@163.com>
2023-10-14 13:55:09 +0800

    update:20231014-01

---
+++ vue2-01-basic/19_单文件组件/Student.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- 组件的结构 -->
+  <div>
+    <h2>学生名称：{{ name }}</h2>
+    <h2>学生年龄：{{ age }}</h2>
+  </div>
+</template>
+
+<script>
+// 组件交互相关的代码（数据、方法等等）
+export default Vue.extend({
+  name: "Student",
+  data() {
+    return {
+      name: "张三",
+      aage: 18,
+    };
+  },
+});
+</script>
