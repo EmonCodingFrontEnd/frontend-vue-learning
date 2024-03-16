9c83ae4d142f0529a9fe0fd4607a90141976c54f
emon <liming20110711@163.com>
2024-03-16 18:32:48 +0800

    update:20240316-01

--- vue2-03-inaction/vue-admin-template/src/router/index.js
+++ vue2-03-inaction/vue-admin-template/src/router/index.js
@@ -1,13 +1,13 @@
 // 引入Vue|Vue-router
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
 // 使用路由差距
-Vue.use(Router)
+Vue.use(Router);
 
 // 引入最外层骨架的一级组件
 /* Layout */
-import Layout from '@/layout'
+import Layout from "@/layout";
 
 /**
  * Note: sub-menu only appear when route children.length >= 1
@@ -36,136 +36,161 @@ import Layout from '@/layout'
  */
 export const constantRoutes = [
   {
-    path: '/login',
-    component: () => import('@/views/login/index'),
-    hidden: true
+    path: "/login",
+    component: () => import("@/views/login/index"),
+    hidden: true,
   },
 
   {
-    path: '/404',
-    component: () => import('@/views/404'),
-    hidden: true
+    path: "/404",
+    component: () => import("@/views/404"),
+    hidden: true,
   },
 
   {
-    path: '/',
+    path: "/",
     component: Layout,
-    redirect: '/dashboard',
-    children: [{
-      path: 'dashboard',
-      name: 'Dashboard',
-      component: () => import('@/views/dashboard/index'),
-      meta: { title: '首页', icon: 'dashboard' }
-    }]
+    redirect: "/dashboard",
+    children: [
+      {
+        path: "dashboard",
+        name: "Dashboard",
+        component: () => import("@/views/dashboard/index"),
+        meta: { title: "首页", icon: "dashboard" },
+      },
+    ],
   },
 
   {
-    path: '/deepSelector',
+    path: "/deepSelector",
     component: Layout,
-    redirect: '/deepSelector',
-    children: [{
-      path: 'deepSelector',
-      name: 'DeepSelector',
-      component: () => import('@/views/DeepSelector深度选择器'),
-      meta: { title: '深度选择器', icon: 'el-icon-s-grid' }
-    }]
+    redirect: "/deepSelector",
+    children: [
+      {
+        path: "deepSelector",
+        name: "DeepSelector",
+        component: () => import("@/views/DeepSelector深度选择器"),
+        meta: { title: "深度选择器", icon: "el-icon-s-grid" },
+      },
+    ],
   },
-]
+
+  {
+    path: "/vue2Test",
+    component: Layout,
+    name: "Vue2Test",
+    redirect: "/vue2Test/cascader",
+    meta: { title: "Vue2演武场", icon: "el-icon-s-grid" },
+    children: [
+      {
+        path: "cascader",
+        name: "Cascader",
+        component: () => import("@/views/Vue2演武场/cascader"),
+        meta: { title: "Cascader级联选择器" },
+      },
+      {
+        path: "cascader2",
+        name: "Cascader2",
+        component: () => import("@/views/Vue2演武场/cascader"),
+        meta: { title: "Cascader级联选择器2" },
+      },
+    ],
+  },
+];
 
 // 异步路由：不同的用户（角色）需要过滤筛选出的路由，称之为异步路由！！！
 export const asyncRoutes = [
   // 权限数据管理相关的路由
   {
-    path: '/acl',
+    path: "/acl",
     component: Layout,
-    name: 'Acl',
-    redirect: '/acl/user/list',
-    meta: { title: '权限管理', icon: 'el-icon-lock' },
+    name: "Acl",
+    redirect: "/acl/user/list",
+    meta: { title: "权限管理", icon: "el-icon-lock" },
     children: [
       {
-        path: 'user/list',
-        name: 'User',
-        component: () => import('@/views/acl/user/list'),
-        meta: { title: '用户管理', }
+        path: "user/list",
+        name: "User",
+        component: () => import("@/views/acl/user/list"),
+        meta: { title: "用户管理" },
       },
       {
-        path: 'role/list',
-        name: 'Role',
-        component: () => import('@/views/acl/role/list'),
-        meta: { title: '角色管理', }
+        path: "role/list",
+        name: "Role",
+        component: () => import("@/views/acl/role/list"),
+        meta: { title: "角色管理" },
       },
       {
-        path: 'role/auth/:id',
-        name: 'RoleAuth',
-        component: () => import('@/views/acl/role/roleAuth'),
-        meta: { title: '角色授权', activeMenu: '/acl/role/list' },
-        hidden: true
+        path: "role/auth/:id",
+        name: "RoleAuth",
+        component: () => import("@/views/acl/role/roleAuth"),
+        meta: { title: "角色授权", activeMenu: "/acl/role/list" },
+        hidden: true,
       },
       {
-        path: 'permission/list',
-        name: 'Permission',
-        component: () => import('@/views/acl/permission/list'),
-        meta: { title: '菜单管理' },
+        path: "permission/list",
+        name: "Permission",
+        component: () => import("@/views/acl/permission/list"),
+        meta: { title: "菜单管理" },
       },
-    ]
+    ],
   },
 
   {
-    path: '/product',
+    path: "/product",
     component: Layout,
-    name: 'Product',
-    meta: { title: '商品管理', icon: 'el-icon-goods' },
+    name: "Product",
+    meta: { title: "商品管理", icon: "el-icon-goods" },
     children: [
       {
-        path: 'trademark',
-        name: 'TradeMark',
-        component: () => import('@/views/Product/TradeMark'),
-        meta: { title: '品牌管理', }
+        path: "trademark",
+        name: "TradeMark",
+        component: () => import("@/views/Product/TradeMark"),
+        meta: { title: "品牌管理" },
       },
       {
-        path: 'attr',
-        name: 'Attr',
-        component: () => import('@/views/Product/Attr'),
-        meta: { title: '平台属性管理', }
+        path: "attr",
+        name: "Attr",
+        component: () => import("@/views/Product/Attr"),
+        meta: { title: "平台属性管理" },
       },
       {
-        path: 'spu',
-        name: 'Spu',
-        component: () => import('@/views/Product/Spu'),
-        meta: { title: 'Spu管理', }
+        path: "spu",
+        name: "Spu",
+        component: () => import("@/views/Product/Spu"),
+        meta: { title: "Spu管理" },
       },
       {
-        path: 'sku',
-        name: 'Sku',
-        component: () => import('@/views/Product/Sku'),
-        meta: { title: 'Sku管理', }
+        path: "sku",
+        name: "Sku",
+        component: () => import("@/views/Product/Sku"),
+        meta: { title: "Sku管理" },
       },
-    ]
+    ],
   },
-]
+];
 
 // 任意路由：当路径出现错误的时候重定向到404
 export const anyRoutes = [
   // 404 page must be placed at the end !!!
-  { path: '*', redirect: '/404', hidden: true },
-]
-
+  { path: "*", redirect: "/404", hidden: true },
+];
 
 const createRouter = () => {
   return new Router({
     // mode: 'history', // require service support
     scrollBehavior: () => ({ y: 0 }),
     // 因为注册的路由是“死的”，“活的”路由是根据不同用户（角色）可以展示不同的菜单。
-    routes: constantRoutes
+    routes: constantRoutes,
   });
-}
+};
 
-const router = createRouter()
+const router = createRouter();
 
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
-  const newRouter = createRouter()
-  router.matcher = newRouter.matcher // reset router
+  const newRouter = createRouter();
+  router.matcher = newRouter.matcher; // reset router
 }
 
-export default router
+export default router;
